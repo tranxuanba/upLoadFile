e5e59f384269a263003c0b10533c97d2df977b9c
ishopjapan <thoitran2107@gmail.com>
2022-09-13 22:23:42 +0700

    up - load file

---
+++ src/main/java/com/macOfBa/service/IProductService.java
@@ -0,0 +1,13 @@
+package com.macOfBa.service;
+
+import com.macOfBa.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+    void save(Product product);
+    Product findById(int id);
+    void update(int id, Product product);
+    void remove(int id);
+}
